345501be09d12c103ad4d8e997702340b9a0e237
Responsibleuk <james@we-are-responsible.com>
2022-02-01 22:45:57 +0000

    Add nav script

---
+++ assets/script/nav.js
@@ -0,0 +1,7 @@
+/* jshint esversion: 8 */
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".menu");
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show-menu");
+});
